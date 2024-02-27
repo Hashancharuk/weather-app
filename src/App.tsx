261b8c9d344564af9f7ac69f31e09a3cca7968a5
HashanCharuk <charukhashan@gmail.com>
2024-02-27 21:38:07 +0530

    first-commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import MainFile from './assests/components/mainFile';
+
+
+function App() {
+ 
+  return (
+    <>
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-6">
+        <MainFile/>
+        <h1>Hashan</h1>
+        </div>
+        <div className="col-lg-6">
+     
+        </div>
+      </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default App
+
